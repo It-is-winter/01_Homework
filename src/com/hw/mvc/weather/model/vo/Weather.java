a5399a6ddfdfbf896c544558d293cc137d7505d1
it-is-winter <seaotter.theatre@gmail.com>
2026-04-11 21:15:10 +0900

    feat: add mvc package and implement weather CRUD functionality

---
+++ src/com/hw/mvc/weather/model/vo/Weather.java
@@ -0,0 +1,5 @@
+package com.hw.mvc.weather.model.vo;
+
+public record Weather(Integer weatherId, String weatherName, String temperature, Long msId) {
+
+}
---
+++ src/com/hw/mvc/weather/run/Run.java
@@ -0,0 +1,12 @@
+package com.hw.mvc.weather.run;
+
+import com.hw.mvc.weather.view.WeatherView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new WeatherView().mainMenu();
+
+	}
+
+}
